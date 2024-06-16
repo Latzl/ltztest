d292611d29d1335f301723bc6844f7b27e60d508
latzl <theaccount_latzl@163.com>
2024-06-16 19:28:42 +0800

    install

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ target_dir='/usr/local/include'
 echo "start copy ltz header files to ${target_dir}"
 cp -rvf ./third_party/ltz/ ${target_dir}/
 echo "start copy tcli header files to ${target_dir}"
-cp -rvf ./tcli/include/ ${target_dir}/tcli/
+cp -rvf ./include/tcli/ ${target_dir}/
