8b76cdd7595c386ad2dcc48551c45aef60a963a2
latzl <theaccount_latzl@163.com>
2024-06-29 16:49:07 +0800

    gtest macro rename

--- example/gtest/example.cpp
+++ example/gtest/example.cpp
@@ -1,32 +1,32 @@
 #include <tcli/gtest.hpp>
 
-TCLI_GTEST_ALL()
+TCLI_GTEST_CALL_ALL()
 
-TCLI_GTEST(a, b, c, a, 1) {
+TCLI_GTEST_DEF(a, b, c, a, 1) {
     std::cout << "a.1" << std::endl;
 }
-TCLI_GTEST_CASE(a, b, c, a, 1)
+TCLI_GTEST_CALL_CASE(a, b, c, a, 1)
 
-TCLI_GTEST(a, b, c, a, 2) {
+TCLI_GTEST_DEF(a, b, c, a, 2) {
     std::cout << "a.2" << std::endl;
 }
-TCLI_GTEST_CASE(a, b, c, a, 2)
+TCLI_GTEST_CALL_CASE(a, b, c, a, 2)
 
-TCLI_GTEST(a, b, c, a, 3) {
+TCLI_GTEST_DEF(a, b, c, a, 3) {
     std::cout << "a.3" << std::endl;
 }
-TCLI_GTEST_CASE(a, b, c, a, 3)
+TCLI_GTEST_CALL_CASE(a, b, c, a, 3)
 
-TCLI_GTEST_SUITE(a, b, c, a)
+TCLI_GTEST_CALL_SUITE(a, b, c, a)
 
-TCLI_GTEST(x, y, z, b, 1) {
+TCLI_GTEST_DEF(x, y, z, b, 1) {
     std::cout << "b.1" << std::endl;
 }
-TCLI_GTEST_CASE(x, y, z, b, 1)
+TCLI_GTEST_CALL_CASE(x, y, z, b, 1)
 
-TCLI_GTEST(x, y, z, b, 2) {
+TCLI_GTEST_DEF(x, y, z, b, 2) {
     std::cout << "b.2" << std::endl;
 }
-TCLI_GTEST_CASE(x, y, z, b, 2)
+TCLI_GTEST_CALL_CASE(x, y, z, b, 2)
 
-TCLI_GTEST_SUITE(x, y, z, b)
+TCLI_GTEST_CALL_SUITE(x, y, z, b)
--- include/tcli/gtest.hpp
+++ include/tcli/gtest.hpp
@@ -34,27 +34,25 @@ int test_with_filter(const std::string& suit = "", const std::string& test = "")
     @brief Define a test case.
     @param ... Variadic that specify case path. Last two params indicate suit name and case name, while remaider combine to path.
  */
-// todo rename to TCLI_GTEST_DEF
-#define TCLI_GTEST(...) _TCLI_GTEST_I(__VA_ARGS__)
+#define TCLI_GTEST_DEF(...) _TCLI_GTEST_DEF_I(__VA_ARGS__)
 
 // call
 /*
     @brief Start test all case. It's suppose to call at global scope if testing all case is considered.
  */
-// todo rename to TCLI_GTEST_CALL_ALL
-#define TCLI_GTEST_ALL() _TCLI_GTEST_ALL_I()
+#define TCLI_GTEST_CALL_ALL() _TCLI_GTEST_CALL_ALL_I()
 
 /*
     @brief Start test suit specified by variadic like (path, suit)
     @param ... Variadic that specify suit path. Last param indicate suit name, while remaider combine to path.
  */
-#define TCLI_GTEST_SUITE(...) _TCLI_GTEST_SUIT_I(__VA_ARGS__)
+#define TCLI_GTEST_CALL_SUITE(...) _TCLI_GTEST_CALL_SUIT_I(__VA_ARGS__)
 
 /*
     @brief Start test case specified by variadic like (path, suit, case)
     @param ... Variadic that specify case path. Last two params indicate suit name and case name, while remaider combine to path.
  */
-#define TCLI_GTEST_CASE(...) _TCLI_GTEST_CASE_I(__VA_ARGS__)
+#define TCLI_GTEST_CALL_CASE(...) _TCLI_GTEST_CALL_CASE_I(__VA_ARGS__)
 
 
 /* definition */
@@ -66,8 +64,8 @@ int test_with_filter(const std::string& suit = "", const std::string& test = "")
 #define _TCLI_GTEST_VA_GET_PATH(...) BOOST_PP_SEQ_ENUM(BOOST_PP_SEQ_FIRST_N(BOOST_PP_SUB(BOOST_PP_VARIADIC_SIZE(__VA_ARGS__), 2), BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__)))
 
 // test define
-#define _TCLI_GTEST_II(suit, case, ...) TEST(_TCLI_GTEST_GEN_SUIT_NAME(suit, __VA_ARGS__), case)
-#define _TCLI_GTEST_I(...) _TCLI_GTEST_II(_TCLI_GTEST_VA_GET_SUIT(__VA_ARGS__), _TCLI_GTEST_VA_GET_CASE(__VA_ARGS__), _TCLI_GTEST_VA_GET_PATH(__VA_ARGS__))
+#define _TCLI_GTEST_DEF_II(suit, case, ...) TEST(_TCLI_GTEST_GEN_SUIT_NAME(suit, __VA_ARGS__), case)
+#define _TCLI_GTEST_DEF_I(...) _TCLI_GTEST_DEF_II(_TCLI_GTEST_VA_GET_SUIT(__VA_ARGS__), _TCLI_GTEST_VA_GET_CASE(__VA_ARGS__), _TCLI_GTEST_VA_GET_PATH(__VA_ARGS__))
 
 // call def
 #define _TCLI_GTEST_CALL_TYPE_ALL 0
@@ -94,7 +92,7 @@ int test_with_filter(const std::string& suit = "", const std::string& test = "")
     LTZ_PI_FN_DEF_MAIN(tcli, gtest, __VA_ARGS__)
 
 // call all
-#define _TCLI_GTEST_ALL_I()                                         \
+#define _TCLI_GTEST_CALL_ALL_I()                                         \
     _TCLI_GTEST_CALL_DEF(_TCLI_GTEST_CALL_TYPE_ALL) {               \
         ::testing::InitGoogleTest(&tcli::argc_raw, tcli::argv_raw); \
         return RUN_ALL_TESTS();                                     \
@@ -103,17 +101,17 @@ int test_with_filter(const std::string& suit = "", const std::string& test = "")
 // call suit
 #define _TCLI_GTEST_SUIT_VA_GET_SUIT(...) BOOST_PP_VARIADIC_ELEM(BOOST_PP_SUB(BOOST_PP_VARIADIC_SIZE(__VA_ARGS__), 1), __VA_ARGS__)
 #define _TCLI_GTEST_SUIT_VA_GET_PATH(...) BOOST_PP_SEQ_ENUM(BOOST_PP_SEQ_FIRST_N(BOOST_PP_SUB(BOOST_PP_VARIADIC_SIZE(__VA_ARGS__), 1), BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__)))
-#define _TCLI_GTEST_SUIT_II(suit, ...)                                                                           \
+#define _TCLI_GTEST_CALL_SUIT_II(suit, ...)                                                                           \
     _TCLI_GTEST_CALL_DEF(_TCLI_GTEST_CALL_TYPE_SUIT, __VA_ARGS__, suit) {                                        \
         return ::tcli::gtest::test_with_filter(BOOST_PP_STRINGIZE(_TCLI_GTEST_GEN_SUIT_NAME(suit, __VA_ARGS__))); \
     }
-#define _TCLI_GTEST_SUIT_I(...) _TCLI_GTEST_SUIT_II(_TCLI_GTEST_SUIT_VA_GET_SUIT(__VA_ARGS__), _TCLI_GTEST_SUIT_VA_GET_PATH(__VA_ARGS__))
+#define _TCLI_GTEST_CALL_SUIT_I(...) _TCLI_GTEST_CALL_SUIT_II(_TCLI_GTEST_SUIT_VA_GET_SUIT(__VA_ARGS__), _TCLI_GTEST_SUIT_VA_GET_PATH(__VA_ARGS__))
 
 // call case
-#define _TCLI_GTEST_CASE_II(suit, case, ...)                                                                                               \
+#define _TCLI_GTEST_CALL_CASE_II(suit, case, ...)                                                                                               \
     _TCLI_GTEST_CALL_DEF(_TCLI_GTEST_CALL_TYPE_CASE, __VA_ARGS__, suit, case) {                                                            \
         return ::tcli::gtest::test_with_filter(BOOST_PP_STRINGIZE(_TCLI_GTEST_GEN_SUIT_NAME(suit, __VA_ARGS__)), BOOST_PP_STRINGIZE(case)); \
     }
-#define _TCLI_GTEST_CASE_I(...) _TCLI_GTEST_CASE_II(_TCLI_GTEST_VA_GET_SUIT(__VA_ARGS__), _TCLI_GTEST_VA_GET_CASE(__VA_ARGS__), _TCLI_GTEST_VA_GET_PATH(__VA_ARGS__))
+#define _TCLI_GTEST_CALL_CASE_I(...) _TCLI_GTEST_CALL_CASE_II(_TCLI_GTEST_VA_GET_SUIT(__VA_ARGS__), _TCLI_GTEST_VA_GET_CASE(__VA_ARGS__), _TCLI_GTEST_VA_GET_PATH(__VA_ARGS__))
 
 #endif
