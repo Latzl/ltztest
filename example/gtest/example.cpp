b3d7c010071ee5eef244e506e18f38e2def5ba4c
latzl <theaccount_latzl@163.com>
2024-07-09 20:42:03 +0800

    rename gtest macro

--- example/gtest/example.cpp
+++ example/gtest/example.cpp
@@ -32,15 +32,15 @@ bool lor(bool a, bool b) {
 }  // namespace logic
 }  // namespace test
 
-TCLI_GTEST_DEF(test, return1) {
+TCLI_GTEST_FN(test, return1) {
     EXPECT_EQ(test::return1(), 1);
 }
 
-TCLI_GTEST_DEF(test, return2) {
+TCLI_GTEST_FN(test, return2) {
     EXPECT_EQ(test::return2(), 2);
 }
 
-TCLI_GTEST_DEF(test, arithmetic, add) {
+TCLI_GTEST_FN(test, arithmetic, add) {
     EXPECT_EQ(test::arithmetic::add(1, 2), 3);
     EXPECT_EQ(test::arithmetic::add(-1, -2), -3);
 
@@ -50,7 +50,7 @@ TCLI_GTEST_DEF(test, arithmetic, add) {
     EXPECT_EQ(test::arithmetic::add(INT32_MAX, INT32_MAX), -2);
 }
 
-TCLI_GTEST_DEF(test, arithmetic, sub) {
+TCLI_GTEST_FN(test, arithmetic, sub) {
     EXPECT_EQ(test::arithmetic::sub(1, 2), -1);
     EXPECT_EQ(test::arithmetic::sub(-1, -2), 1);
 
@@ -60,36 +60,36 @@ TCLI_GTEST_DEF(test, arithmetic, sub) {
     EXPECT_EQ(test::arithmetic::sub(INT32_MAX, INT32_MAX), 0);
 }
 
-TCLI_GTEST_DEF(test, logic, land) {
+TCLI_GTEST_FN(test, logic, land) {
     EXPECT_EQ(test::logic::land(false, false), false);
     EXPECT_EQ(test::logic::land(false, true), false);
     EXPECT_EQ(test::logic::land(true, false), false);
     EXPECT_EQ(test::logic::land(true, true), true);
 }
 
-TCLI_GTEST_DEF(test, logic, lor) {
+TCLI_GTEST_FN(test, logic, lor) {
     EXPECT_EQ(test::logic::lor(false, false), false);
     EXPECT_EQ(test::logic::lor(false, true), true);
     EXPECT_EQ(test::logic::lor(true, false), true);
     EXPECT_EQ(test::logic::lor(true, true), true);
 }
 
-TCLI_GTEST_DEF(other, a, b) {
+TCLI_GTEST_FN(other, a, b) {
     std::cout << "other/a/b" << std::endl;
 }
 
-TCLI_GTEST_DEF(other, a, c) {
+TCLI_GTEST_FN(other, a, c) {
     std::cout << "other/a/c" << std::endl;
 }
 
-TCLI_GTEST_DEF(other, b, a) {
+TCLI_GTEST_FN(other, b, a) {
     std::cout << "other/b/a" << std::endl;
 }
 
-TCLI_GTEST_DEF(other, b, c) {
+TCLI_GTEST_FN(other, b, c) {
     std::cout << "other/b/c" << std::endl;
 }
 
-TCLI_GTEST_DEF(other, b, d) {
+TCLI_GTEST_FN(other, b, d) {
     std::cout << "other/b/d" << std::endl;
 }
--- include/tcli/gtest.hpp
+++ include/tcli/gtest.hpp
@@ -32,23 +32,22 @@ struct node : public basic_node {
         For test suit recursively, specify path not point to suit node.
         All path should start with 'gtest' as first node.
         For example:
-            TCLI_GTEST_DEF(a, b, c)
-            TCLI_GTEST_DEF(a, b, d)
-            TCLI_GTEST_DEF(a, e, f)
-            TCLI_GTEST_DEF(g, h)
+            TCLI_GTEST_FN(a, b, c)
+            TCLI_GTEST_FN(a, b, d)
+            TCLI_GTEST_FN(a, e, f)
+            TCLI_GTEST_FN(g, h)
             `tcli gtest` will test all four case.
             `tcli gtest a b c` will test case c
             `tcli gtest a b` will test suit a.
             `tcli gtest a` will test case under a.
  */
-#define TCLI_GTEST_DEF(...) _TCLI_GTEST_DEF_I(__VA_ARGS__)
+#define TCLI_GTEST_FN(...) _TCLI_GTEST_FN_I(__VA_ARGS__)
 
 /* definition */
 // common
 #define _TCLI_GTEST_CALL_TYPE_CASE 0
 #define _TCLI_GTEST_CALL_TYPE_CASE_INST ::tcli::gtest::node::test_type::case_
 
-#define _TCLI_FN_GTEST(...) TCLI_FN(gtest, __VA_ARGS__)
 #define _TCLI_GTEST_GEN_SUIT_NAME(suit, ...) LTZ_PP_VA_CAT_WITH_UNDERLINE(__VA_ARGS__, suit)
 #define _TCLI_GTEST_VA_GET_SUIT(...) BOOST_PP_VARIADIC_ELEM(BOOST_PP_SUB(BOOST_PP_VARIADIC_SIZE(__VA_ARGS__), 2), __VA_ARGS__)
 #define _TCLI_GTEST_VA_GET_CASE(...) BOOST_PP_VARIADIC_ELEM(BOOST_PP_SUB(BOOST_PP_VARIADIC_SIZE(__VA_ARGS__), 1), __VA_ARGS__)
@@ -56,7 +55,7 @@ struct node : public basic_node {
 
 
 // test define
-#define _TCLI_GTEST_DEF_III(suit, case, ...)                                       \
+#define _TCLI_GTEST_FN_III(suit, case, ...)                                        \
     LTZ_PI_FN_NODE_CONSTRUCT(tcli, ::tcli::gtest::node, __VA_ARGS__, suit, case) { \
         type = _TCLI_GTEST_CALL_TYPE_CASE_INST;                                    \
     }                                                                              \
@@ -64,7 +63,7 @@ struct node : public basic_node {
     LTZ_PI_FN_DEF_CLEAN(tcli, __VA_ARGS__, suit, case) {}                          \
     LTZ_PI_FN_DEF_MAIN(tcli, __VA_ARGS__, suit, case) {}                           \
     TEST(_TCLI_GTEST_GEN_SUIT_NAME(suit, __VA_ARGS__), case)
-#define _TCLI_GTEST_DEF_II(...) _TCLI_GTEST_DEF_III(_TCLI_GTEST_VA_GET_SUIT(__VA_ARGS__), _TCLI_GTEST_VA_GET_CASE(__VA_ARGS__), _TCLI_GTEST_VA_GET_PATH(__VA_ARGS__))
-#define _TCLI_GTEST_DEF_I(...) _TCLI_GTEST_DEF_II(gtest, __VA_ARGS__)
+#define _TCLI_GTEST_FN_II(...) _TCLI_GTEST_FN_III(_TCLI_GTEST_VA_GET_SUIT(__VA_ARGS__), _TCLI_GTEST_VA_GET_CASE(__VA_ARGS__), _TCLI_GTEST_VA_GET_PATH(__VA_ARGS__))
+#define _TCLI_GTEST_FN_I(...) _TCLI_GTEST_FN_II(gtest, __VA_ARGS__)
 
 #endif
