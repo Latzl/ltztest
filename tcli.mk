68d68508bf2ad199afb83ec618abff94bc017775
latzl <theaccount_latzl@163.com>
2024-10-20 19:32:15 +0800

    tcli.mk

--- tcli.mk
+++ tcli.mk
@@ -1,15 +1,18 @@
 TCLI_MKDIR = $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))))
 CXX = g++
-FLAGS = -g -O0 -std=c++11 -Wall
-BUILD_DIR = ./build
+FLAGS = -g -O0 -std=c++14 -Wall
+BUILD_DIR = $(THIS_MKDIR)/build
 OBJ_DIR = $(BUILD_DIR)/obj
 TARGET_DIR = $(BUILD_DIR)/bin
 TARGET = $(TARGET_DIR)/tcli
+FLAGS += -fno-access-control
+
+INCS +=
 
 LIBS += -Wl,-Bstatic -ltcli -Wl,-Bdynamic
-LIBS += -lpthread -lrt
+LIBS += -lpthread -lrt -ldl
 
-HEADERS += $(wildcard $(TCLI_MKDIR)/third_party/ltz/*/*.hpp)
+# HEADERS += $(wildcard $(TCLI_MKDIR)/third_party/ltz/*/*.hpp)
 HEADERS += $(wildcard $(TCLI_MKDIR)/include/tcli/*.hpp)
 
 # call GET_OBJS,src_dir,created_obj_dir
@@ -21,23 +24,25 @@ endef
 OBJS += $(call GET_OBJS,./gtest/,gtest)
 $(OBJ_DIR)/gtest/%.o: ./gtest/%.cpp $(HEADERS)
 	@mkdir -p $(OBJ_DIR)/gtest
-	$(CXX) $(FLAGS) -c $< -o $@
+	$(CXX) $(INCS) $(FLAGS) -c $< -o $@
 
 # .
+HEADERS += $(wildcard ./*.hpp)
 OBJS += $(call GET_OBJS,./,.)
 $(OBJ_DIR)/%.o: ./%.cpp $(HEADERS)
 	@mkdir -p $(OBJ_DIR)
-	$(CXX) $(FLAGS) -c $< -o $@
+	$(CXX) $(INCS) $(FLAGS) -c $< -o $@
 
-PATH2TCLILIB = $(TCLI_MKDIR)/src/tcli/build/bin/libtcli.a
+# PATH2TCLILIB = $(TCLI_MKDIR)/src/tcli/build/bin/libtcli.a
 
 $(TARGET): $(OBJS)
 	@mkdir -p $(TARGET_DIR)
-	$(CXX) $(FLAGS) -o $@ $^ $(LIBS)
+	$(CXX) $(INCS) $(FLAGS) -o $@ $^ $(LIBS)
 
-$(OBJS): $(PATH2TCLILIB)
+# $(OBJS): $(PATH2TCLILIB)
 
 .PHONY: all clean test
+.DEFAULT_GOAL: all
 
 all : $(TARGET)
 
@@ -45,3 +50,5 @@ clean:
 	rm -r $(BUILD_DIR)
 
 test:
+	@echo $(THIS_MKDIR)
+	@echo $(BUILD_DIR)
